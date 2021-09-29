07ac214f3c01275232f0831fec7080bb83e3be2b
Kiran Vaddi <pozzolabadmin@Chemicals-MacBook-Pro.local>
2021-09-29 15:32:25 -0700

    add example test case for shapematching

--- head/__init__.py
+++ head/__init__.py
@@ -5,3 +5,4 @@ from .policies import thompson_sampling
 from .utils import get_spectrum, ExampleRunnerSimulation, ExampleRunner
 from .metrics import euclidean_dist
 from .modelling import Emulator, EmulatorMultiShape
+from .expts import TestShapeMatchBO
