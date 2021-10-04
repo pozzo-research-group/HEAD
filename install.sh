e69eff7cb1d4249e7248c34443eb08097d4a2e5b
Kiran Vaddi <pozzolabadmin@Chemicals-MacBook-Pro.local>
2021-10-04 16:33:24 -0700

    local installation complete

--- install.sh
+++ install.sh
@@ -2,6 +2,13 @@ python3 -m venv env
 source env/bin/activate
 python3 -m pip install -r require.txt
 pip install -e .
+mkdir ext
+git clone https://github.com/GAA-UAM/scikit-fda.git
+pip install ./scikit-fda
+git clone https://gitlab.com/wiechapeter/pyGDM2.git
+pip install ./pyGDM2
+git clone https://github.com/pytorch/botorch.git
+pip install ./botorch
 pip install ipykernel
 python3 -m ipykernel install --user --name head --display-name "head"
 
