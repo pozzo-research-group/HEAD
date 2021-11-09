72360c159c37dfa7b736609e5f4facff91a1305a
Kiran Vaddi <pozzolabadmin@Chemicals-MacBook-Pro.local>
2021-11-09 10:55:13 -0800

    modify install.sh and related files and remove unnecessary installs

--- install.sh
+++ install.sh
@@ -3,10 +3,6 @@ source env/bin/activate
 python3 -m pip install -r require.txt
 pip install -e .
 mkdir ext
-git clone https://github.com/GAA-UAM/scikit-fda.git
-pip install ./scikit-fda
-git clone https://gitlab.com/wiechapeter/pyGDM2.git
-pip install ./pyGDM2
 git clone https://github.com/pytorch/botorch.git
 pip install ./botorch
 pip install ipykernel
